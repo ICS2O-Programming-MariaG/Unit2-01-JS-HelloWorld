e5451a8addb0f0b153aff02387a0509da2c3cb28
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-03-09 16:05:15 +0000

    Added style.css and script.js files

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2022 Maria Goemans All rights reserved
+//
+// Created by: Maria Goemans
+// Created on: Mar 9, 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  // this function displays "Hello, World!"
+  document.getElementById('hello-world').innerHTML = 'Hello, world! I coded this function in Javascript!'
+}
