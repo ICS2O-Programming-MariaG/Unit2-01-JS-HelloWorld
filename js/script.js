b5fba4ae3fbdf9681234debc34f5a8616a84e21b
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-03-10 15:50:48 +0000

    Added comments

--- js/script.js
+++ js/script.js
@@ -4,7 +4,12 @@
 // Created on: Mar 9, 2022
 // This file contains the JS functions for index.html
 
-function myButtonClicked () {
+function myButtonClicked() {
   // this function displays "Hello, World!"
   document.getElementById('hello-world').innerHTML = 'Hello, world! I coded this function in Javascript!'
+}
+
+function ButtonTwo() {
+  // this function replaces the above text with a blank space, 'hiding' the text
+  document.getElementById('hello-world') .innerHTML=" "  
 }
